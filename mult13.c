8d8d51852c8af32fe433291e94977444a410512f
anickelson03 <46019633+anickelson03@users.noreply.github.com>
2019-02-26 12:42:19 +0530

    Create mult13.c

---
+++ mult13.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a=13,b,c;
+printf("enter the value");
+scanf("%d",&b);
+if(a!0)
+{
+c=a+b;
+if(c>a)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
+return 0;
+}
